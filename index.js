0c0d512df3d6a6825c98ecab7204c62ad9cb55b5
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-02-13 10:58:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function createPost(){
   var pageTemplate = document.getElementById('page-template').innerHTML;
   var pageDiv = document.getElementById('page');
   var pageFn = _.template(pageTemplate);
-  var pageHTML = pageFn;
+  var pageHTML = pageFn({'postTitle': "Matija Posts"});
   pageDiv.innerHTML += pageHTML;
 
 }
