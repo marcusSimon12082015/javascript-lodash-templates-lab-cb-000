09437df9e44d4f8d9b3eb169efe252d8f0946ab3
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-02-13 11:30:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ function createPost(){
   pageDiv.innerHTML += pageHTML;
 
   //get values from form post
+  var postTitle = document.getElementById('postTitle').value;
+  var postBody = document.getElementById('postBody').value;
+  var postAuthor = document.getElementById('postAuthor').value;
 
+  var postTemplate = document.getElementById('post-template').innerHTML;
+  var pagePostDiv = document.getElementById('post');
+  var postFunction = _.template(postTemplate);
+  var postHTML = postFunction({'postTitle':postTitle,'postBody':postBody,'postAuthor': postAuthor});
+  pagePostDiv.innerHTML += postHTML;
+   
 
 }
 
