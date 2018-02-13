b861ceedf104b806f340ec4574a9024d973b257c
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-02-13 10:52:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function createPost(){
   var pageTemplate = document.getElementById('page-template').innerHTML;
   var pageDiv = document.getElementById('page');
-  debugger;
   var pageFn = _.template(pageTemplate);
   var pageHTML = pageFn;
   pageDiv.innerHTML += pageHTML;
+  //debugger;
 
 
 
