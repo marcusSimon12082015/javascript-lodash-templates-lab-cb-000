341206bcc93f9f8e8a16952c603c452f3d08a197
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-02-13 10:08:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function createPost(){
+  var pageTemplate = document.getElementById('page-template').innerHTML;
+  var pageFn = _.template(pageTemplate);
 
+  var pageDiv = document.getElementById('page');
 }
 
 function postComment(){
